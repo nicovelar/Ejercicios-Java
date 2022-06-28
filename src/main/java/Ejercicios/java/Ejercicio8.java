c964fec4cc662dd9b89c49bf84dec5cfc94f8c2a
nicovelar <nicovelar@gmail.com>
2022-06-28 12:46:47 -0300

    Ejercicio 8

---
+++ src/main/java/Ejercicios/java/Ejercicio8.java
@@ -0,0 +1,31 @@
+package Ejercicios.java;
+
+import java.util.Scanner;
+
+public class Ejercicio8 {
+
+    public static void main(String[] args) {
+
+        //Declaración de variables
+        Scanner scan=new Scanner(System.in);
+        String diaSemana;
+
+        System.out.println("Seleccione entre: Lunes, Martes, Miércoles, Jueves, Viernes, Sábado o Domingo");
+        diaSemana = scan.nextLine();
+        switch (diaSemana) {
+            case "Lunes":
+            case "Martes":
+            case "Miércoles":
+            case "Jueves":
+            case "Viernes":
+                System.out.println("Día laborable");
+                break;
+            case "Sábado":
+            case "Domingo":
+                System.out.println("Día no laborable");
+                break;
+        }
+
+
+    }
+}
