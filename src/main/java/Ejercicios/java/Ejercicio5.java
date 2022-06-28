f6523adef0f30fbc76a3b1281e416b8658947100
nicovelar <nicovelar@gmail.com>
2022-06-28 12:28:46 -0300

    Ejercicio 5

---
+++ src/main/java/Ejercicios/java/Ejercicio5.java
@@ -0,0 +1,15 @@
+package Ejercicios.java;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+
+        Integer numero = 1;
+        while (numero <101) {
+            System.out.println(numero);
+            numero = numero +1;
+        }
+    }
+}
