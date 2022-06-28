f4d6407b9cb5b0604f7e7eb3b0268101670f8fe7
nicovelar <nicovelar@gmail.com>
2022-06-28 12:57:02 -0300

    Ejercicio 9

---
+++ src/main/java/Ejercicios/java/Ejercicio9.java
@@ -0,0 +1,18 @@
+package Ejercicios.java;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+
+    public static void main(String[] args) {
+
+        //Declaración de variables
+        String texto;
+        String e;
+
+        texto = "La sonrisa sera la mejor arma contra la tristeza";
+        e = texto.replace("a","e");
+        System.out.println(e + ", toda acción acarrea consecuencias.");
+    }
+
+}
