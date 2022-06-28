8bddecf85b089abc3b27396f696b35fcd2c62291
nicovelar <nicovelar@gmail.com>
2022-06-28 12:31:24 -0300

    Ejercicio 6

---
+++ src/main/java/Ejercicios/java/Ejercicio6.java
@@ -0,0 +1,12 @@
+package Ejercicios.java;
+
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+
+        Integer numero;
+        for (numero =1; numero <101; numero++) {
+            System.out.println(numero);
+        }
+    }
+}
